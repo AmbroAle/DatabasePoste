6fb7d89f1345327938a7f5a8d3fd34370f900b50
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-08-09 15:23:01 +0200

    Aggiunti nuovi controlli e metodi per aggiornare dati utente

--- PosteItaliane/Pages/ModificaDati.xaml.cs
+++ PosteItaliane/Pages/ModificaDati.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,77 @@ namespace PosteItaliane.Pages
         {
             InitializeComponent();
         }
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow?.NavigateToPage(new Home());
+        }
+        private void btnAggiorna_Click(object sender, RoutedEventArgs e)
+        {
+            // Connessione al database
+            string connectionString = "server=localhost;uid=root;pwd=;database=PosteItalianeDatabase";
+            bool emailChanged = false;
+            bool passwordChanged = false;
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Valori dei campi
+                string email = txtEmail.Text.Trim();//Trim rimuove gli spazi bianchi all'inizio e alla fine di una stringa
+                string password = txtPassword.Password.Trim();
+
+                // Costruzione della query SQL dinamica
+                var updateQuery = new StringBuilder("UPDATE account SET ");
+                var parameters = new List<MySqlParameter>();
+
+                if (!string.IsNullOrEmpty(email))
+                {
+                    updateQuery.Append("Email = @Email, ");
+                    parameters.Add(new MySqlParameter("@Email", email));
+                    emailChanged = true;
+                }
+                if (!string.IsNullOrEmpty(password))
+                {
+                    updateQuery.Append("Password = @Password, ");
+                    parameters.Add(new MySqlParameter("@Password", password));
+                    passwordChanged = true;
+                }
+
+                // Rimuovi l'ultima virgola e aggiungi la clausola WHERE
+                if (updateQuery.Length > 0)
+                {
+                    updateQuery.Length -= 2; // Rimuove l'ultima virgola
+                    updateQuery.Append(" WHERE CF = @CF");
+
+                    // Aggiungi il parametro CF
+                    parameters.Add(new MySqlParameter("@CF", "ABCDEF12G34H567I")); // Sostituisci con il CF dell'utente attuale
+
+                    // Esegui l'aggiornamento
+                    using (var command = new MySqlCommand(updateQuery.ToString(), connection))
+                    {
+                        command.Parameters.AddRange(parameters.ToArray());
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (emailChanged && passwordChanged)
+                        {
+                            MessageBox.Show("La email e la password sono state aggiornate");
+                        }
+                        else if (passwordChanged == false && emailChanged == true)
+                        {
+                            MessageBox.Show("La email è stata aggiornata");
+                        }
+                        else
+                        {
+                            MessageBox.Show("La password è stata aggiornata");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nessun campo da aggiornare.");
+                }
+            }
+        }
+
+
     }
 }
