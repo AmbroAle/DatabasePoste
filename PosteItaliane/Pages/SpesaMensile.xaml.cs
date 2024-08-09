81d46af4b2712655375bc6632d044a1419705463
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-08-09 15:25:54 +0200

    added monthly expense

--- PosteItaliane/Pages/SpesaMensile.xaml.cs
+++ PosteItaliane/Pages/SpesaMensile.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,64 @@ namespace PosteItaliane.Pages
         public SpesaMensile()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string connectionString = "server=localhost;uid=root;pwd=8323;database=PosteItalianeDatabase"; 
+            DateTime now = DateTime.Now;
+            DateTime startOfMonth = new DateTime(now.Year, now.Month, 1);
+            DateTime endOfMonth = startOfMonth.AddMonths(1).AddDays(-1);
+
+            string query = @"
+                SELECT t.CodTransazione, t.Importo, t.Data, tt.Tipo 
+                FROM TRANSAZIONE t
+                INNER JOIN TIPO_TRANSAZIONE tt ON t.CodTransazione = tt.CodTransazione
+                WHERE t.Data >= @startOfMonth AND t.Data <= @endOfMonth AND t.NumeroIdentificativo = @numeroIdentificativo;";
+
+            DataTable dataTable = new DataTable();
+            decimal totalImporto = 0;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@startOfMonth", startOfMonth);
+                        command.Parameters.AddWithValue("@endOfMonth", endOfMonth);
+                        command.Parameters.AddWithValue("@numeroIdentificativo", UserSession.Instance.NumeroIdentificativo);
+
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                        adapter.Fill(dataTable);
+                    }
+
+                    // Calcolo dell'importo totale
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        totalImporto += Convert.ToDecimal(row["Importo"]);
+                    }
+
+                    // Visualizzare i dati nella MovimentiDataGrid
+                    MovimentiDataGrid.ItemsSource = dataTable.DefaultView;
+
+                    // Mostrare l'importo totale in una label o in un altro controllo
+                    ImportoTotale.Text = $"Importo Totale: {totalImporto:C}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore durante il caricamento dei dati: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnVisualizza_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow?.NavigateToPage(new Home());
         }
     }
 }
